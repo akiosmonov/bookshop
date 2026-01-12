145270fca1024e25a7b7998bcae06ae4db74bb8d
akiosmonov <darken452@gmail.com>
2026-01-12 20:52:13 +0600

    adaptiveForOrder

--- src/Companents/Order/index.jsx
+++ src/Companents/Order/index.jsx
@@ -3,43 +3,45 @@ import { motion } from "framer-motion";
 
 const Order = () => {
   return (
-    <div className=" p-30">
-      <div className="container">
-        <div className=" flex justify-around">
-          <div className=" flex flex-col gap-50">
+    <div className=" p-5 bg-gray-50 min-h-screen">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 items-start">
+          <div className="flex-col md:flex gap-20 md:gap-50">
             <div className=" flex flex-col gap-4">
-              <h1 className="text-[#010049] text-2xl font-bold ">
+              <h1 className="text-[#010049] text-lg md:text-2xl font-bold ">
                 Контакные данные
               </h1>
               <input
                 type="text"
                 placeholder="Фио*"
-                className="w-70 border-2  rounded-[3px] pl-3 pt-1 pb-1 border-[#010049]"
+                className="w-full border-2  rounded-[3px] pl-3 pt-1 pb-1 border-[#010049] focus:ring-2 ring-blue-200 outline-none"
               />
               <input
-                type="text"
+                type="tel"
                 placeholder="Телефон*"
-                className="w-70 border-2  rounded-[3px] pl-3 pt-1 pb-1 border-[#010049]"
+                className=" w-full border-2  rounded-[3px] pl-3 pt-1 pb-1 border-[#010049] focus:ring-2 ring-blue-200 outline-none"
               />
             </div>
             <div className=" flex  flex-col gap-5">
-              <h1 className="text-[#010049] text-2xl font-bold ">Оплата</h1>
+              <h1 className="text-[#010049] text-lg md:text-2xl font-bold ">
+                Оплата
+              </h1>
               <div className=" flex gap-4 items-center ">
-                <input type="checkbox" className="w-5 h-5 accent-[#010049]" />
-                <h2 className="text-[#010049] ">
+                <input type="checkbox" name="payment" className="w-5 h-5 rounded-full appearance-none border-2 border-[#010049] checked:bg-[#010049] transition-all cursor-pointer" />
+                <h2 className="text-[#010049] group-hover:underline">
                   Оплачу наличными при получении <br /> заказа
                 </h2>
               </div>
               <div className=" flex flex-col gap-2 ">
                 <h1 className="text-[#010049]">
                   Оплата с банковской картой через <br />{" "}
-                  <span className=" text-green-500 font-medium">PayBox</span>
+                  <span className=" text-green-600 font-bold tracking-wide">PayBox</span>
                 </h1>
                 <motion.button
-                  whileTap={{ scale: 0.9 }}
+                  whileTap={{ scale: 0.96 }}
                   initial={{ opacity: 0, y: 10 }}
                   animate={{ opacity: 1, y: 0 }}
-                  className="bg-[#010049] text-white w-70 h-8 rounded-[3px]"
+                  className="bg-[#010049] w-full  text-white  h-8 rounded-[3px]"
                 >
                   Оплатить
                 </motion.button>
@@ -47,20 +49,22 @@ const Order = () => {
             </div>
           </div>
           <div className=" flex flex-col gap-15">
-            <div className=" flex flex-col gap-10">
+            <div className=" flex flex-col gap-20">
               <div className=" flex  flex-col gap-1">
-                <h1 className="text-[#010049] text-2xl font-bold ">Доставка</h1>
+                <h1 className="text-[#010049] text-lg md:text-2xl font-bold ">
+                  Доставка
+                </h1>
                 <p className="text-[#000000E5]">
                   Выберите удобный способ доставки <br /> для этого заказа.
                 </p>
               </div>
               <div className=" flex flex-col gap-3">
                 <div className=" flex gap-4 items-center">
-                  <input type="checkbox" className="w-5 h-5 accent-[#010049]" />
+                  <input type="checkbox" className="w-5 h-5 rounded-full appearance-none border-2 border-[#010049] checked:bg-[#010049] transition-all cursor-pointer" />
                   <h1 className="text-[#010049] font-medium">Самовывоз</h1>
                 </div>
                 <div className=" flex gap-4 items-center">
-                  <input type="checkbox" className="w-5 h-5 accent-[#010049]" />
+                  <input type="checkbox" className="w-5 h-5 rounded-full appearance-none border-2 border-[#010049] checked:bg-[#010049] transition-all cursor-pointer" />
                   <h1 className="text-[#010049] font-medium">
                     Доставка курьером
                   </h1>
@@ -68,10 +72,10 @@ const Order = () => {
               </div>
               <textarea
                 placeholder="Область, город (район, село), улица, дом№, кв.№*"
-                className="w-full pr-30 pt-5 pl-10 pb-20 border-2  border-[#010049] outline-none resize-none"
+                className="w-full pt-5 pl-10 pb-20 border-2  border-[#010049] outline-none resize-none transition-colors"
               />
             </div>
-            <div className="bg-[#010049] p-5 flex flex-col gap-5">
+            <div className="bg-[#010049] w-full  p-5 flex flex-col gap-5">
               <div className=" flex justify-between">
                 <h1 className="text-white">Общая сумма:</h1>
                 <h2 className="text-white">3000$</h2>
